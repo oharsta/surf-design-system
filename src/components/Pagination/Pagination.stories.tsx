77d212b7d260663ae449d500e3097986a111fd8f
Okke Harsta <oharsta@zilverline.com>
2023-01-11 15:45:39 +0100

    WIP on re-use scss

---
+++ src/components/Pagination/Pagination.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+
+import Pagination, {defaultButtonProps} from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'ReactComponentLibrary/Button',
+    component: Pagination,
+    args: {
+        ...defaultButtonProps
+    }
+} as ComponentMeta<typeof Pagination>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Pagination> = (args) => <Pagination {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+    txt: 'Default'
+};
+
+export const PrimaryDisabled = Template.bind({});
+PrimaryDisabled.args = {
+    txt: 'Primary disabled',
+    disabled: true
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    txt: 'Secondary',
+    className: 'secondary'
+};
+
+export const SecondaryDisabled = Template.bind({});
+SecondaryDisabled.args = {
+    txt: 'Secondary disabled',
+    className: 'secondary',
+    disabled: true
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+    txt: 'Tertiary',
+    className: 'tertiary',
+};
+
+export const TertiaryDisabled = Template.bind({});
+TertiaryDisabled.args = {
+    txt: 'Tertiary disabled',
+    className: 'tertiary',
+    disabled: true
+};
