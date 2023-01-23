e27076575ce7b5156e56b46bcfceb8b076c482da
Okke Harsta <oharsta@zilverline.com>
2023-01-23 08:58:17 +0100

    BSR

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -3,19 +3,20 @@ import "./Tooltip.scss";
 import {Tooltip as ReactTooltip} from "react-tooltip";
 import {ReactComponent as InfoIcon} from "../../icons/functional-icons/info.svg";
 import DOMPurify from "dompurify";
+import {pseudoGuid} from "../../common/utils";
 
 export interface TooltipProps {
-    anchorId: string;
     tip: string;
+    anchorId?: string;
     clickable?: boolean
 }
 
 const Tooltip = (props: TooltipProps) => {
-
+    const uniqueAnchorId = props.anchorId || pseudoGuid();
     return (
         <div className="sds--tooltip-container">
-            <InfoIcon id={props.anchorId}/>
-            <ReactTooltip anchorId={props.anchorId}
+            <InfoIcon id={uniqueAnchorId}/>
+            <ReactTooltip anchorId={uniqueAnchorId}
                           clickable={props.clickable || false}
                           html={DOMPurify.sanitize(props.tip)}/>
         </div>
