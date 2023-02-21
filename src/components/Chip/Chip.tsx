f5f22804c0991db07da5a7fce6e389ac4edf06a1
Okke Harsta <oharsta@zilverline.com>
2023-02-21 14:32:47 +0100

    Chip styling

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -10,9 +10,9 @@ const Chip = (props: React.PropsWithChildren<ChipProps>) => {
 
     return (
         <button className={`sds--chips ${props.isActive ? "is-active" : ""}`}>
-            <span className="sds--chips--visual">
-                {props.children && props.children}
-            </span>
+            {props.children && <span className="sds--chips--visual">
+                {props.children}
+            </span>}
             <span className="sds--chips--textual sds--text--body--small">{props.label}</span>
         </button>
     );
