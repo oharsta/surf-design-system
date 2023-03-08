9966f6d58c27a89c5c067d9651af237381e9b10a
Okke Harsta <oharsta@zilverline.com>
2023-03-08 15:39:34 +0100

    Chevron ButtonType

--- src/components/Button/ButtonType.tsx
+++ src/components/Button/ButtonType.tsx
@@ -7,7 +7,8 @@ export enum ButtonType {
     GhostDark = "sds--btn--ghost--dark",
     GhostLight = "sds--btn--ghost--light",
     Delete = "sds--btn--delete",
-    DeleteSecondary = "sds--btn--delete--secondary"
+    DeleteSecondary = "sds--btn--delete--secondary",
+    Chevron = "sds--btn--chevron"
 }
 
 export default ButtonType;
